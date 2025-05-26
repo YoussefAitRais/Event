b8865dab1eb85c24db8a27121088b60a531d19b1
Youssef Ait Rais <copybuisness007@gmail.com>
2025-05-26 17:57:06 +0200

    JWT filter

--- src/main/java/org/event/event/Config/SecurityConfig.java
+++
@@ -1,4 +0,0 @@
-package org.event.event.Config;
-
-public class SecurityConfig {
-}
--- src/main/java/org/event/event/Security/SecurityConfig.java
+++ src/main/java/org/event/event/Security/SecurityConfig.java
@@ -1,39 +1,30 @@
 package org.event.event.Security;
 
+import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.builder()
-                .username("admin")
+        UserDetails admin = User.withUsername("admin")
                 .password(passwordEncoder().encode("password"))
                 .roles("ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(user);
-    }
-
-
-    @Bean
-    public UserDetailsService userDetailsServiceBean() {
-        UserDetails user = User.withUsername("Admin")
-                .password(passwordEncoder().encode("12345")) // {noop} pour dire "pas encodé"
-                .authorities("read")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+        return new InMemoryUserDetailsManager(admin);
     }
 
     @Bean
@@ -41,26 +32,20 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.httpBasic().disable();
-//        http.authorizeHttpRequests().anyRequest().authenticated();
-//        return http.build();
-//
-//    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth" , "/register").permitAll()
+                        .requestMatchers("/auth", "/register").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(withDefaults -> {})
-                .httpBasic(withDefaults -> {});
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
+
 
         return http.build();
     }
-
 }
